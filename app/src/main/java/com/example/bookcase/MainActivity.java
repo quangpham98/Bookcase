0d8da56e7cae8b9dedbf9a7a8ff764d4a3535113
Quang Pham <quangphamtran98@gmail.com>
2019-03-25 22:20:51 -0400

    First comit

--- app/src/main/java/com/example/bookcase/MainActivity.java
+++ app/src/main/java/com/example/bookcase/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.bookcase;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BookListFragment.getBookName {
+
+    private boolean isTwoPane;
+
+    BookListFragment bookListFragment;
+    BookDetailsFragmentLandscape bookDetailsFragmentLandscape;
+    ViewPagerAdapter viewPagerAdapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isTwoPane = findViewById(R.id.bookListMainView) != null;
+
+
+        if (isTwoPane) {
+            bookListFragment = new BookListFragment();
+            bookDetailsFragmentLandscape = new BookDetailsFragmentLandscape();
+
+            getSupportFragmentManager().beginTransaction().add(R.id.bookListMainView, bookListFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.bookDetailsMainView, bookDetailsFragmentLandscape).commit();
+
+        } else {
+            viewPager = findViewById(R.id.viewPager);
+            viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), this);
+            viewPager.setAdapter(viewPagerAdapter);
+        }
+    }
+
+    @Override
+    public void bookSelected(String bookName) {
+        bookDetailsFragmentLandscape.displayBookName(bookName);
     }
 }
---
+++ app/src/main/java/com/example/bookcase/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.bookcase;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+    private String[] bookList;
+    BookDetailsFragment bookDetailsFragment;
+    public ViewPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        bookList = context.getResources().getStringArray(R.array.books);
+        bookDetailsFragment = new BookDetailsFragment();
+        bookDetailsFragment.setBookName(bookList[i]);
+        return bookDetailsFragment;
+    }
+
+    @Override
+    public int getCount() {
+
+        bookList = context.getResources().getStringArray(R.array.books);
+        return bookList.length;
+    }
+}
