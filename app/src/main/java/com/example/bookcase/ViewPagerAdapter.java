7ae18d7cc5d1f3edac9473ab7176f07417439ac6
Quang Pham <quangphamtran98@gmail.com>
2019-04-05 23:05:18 -0400

    Fixed view pager adapter.

--- app/src/main/java/com/example/bookcase/ViewPagerAdapter.java
+++ app/src/main/java/com/example/bookcase/ViewPagerAdapter.java
@@ -3,12 +3,12 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 
 import java.util.ArrayList;
 
-public class ViewPagerAdapter extends FragmentPagerAdapter {
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
 
     private ArrayList<Book> bookList;
